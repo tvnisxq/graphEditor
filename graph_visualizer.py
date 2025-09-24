80e44a752783662c48625918ea1adc019235a2af
Tanishq Sharma <ponasanthem@gmail.com>
2025-09-24 21:33:50 +0530

    updated visualizer

--- graph_visualizer.py
+++ graph_visualizer.py
@@ -3,3 +3,12 @@ import networkx as nx
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import graph_algorithms as alg
 import tkinter as tk
+
+class GraphVisualizer:
+    def __init__(self, root, graph):
+        self.root = root
+        self.graph = graph 
+        self.pos = nx.spring_layout(graph)
+        self.visited_nodes = []
+
+        
