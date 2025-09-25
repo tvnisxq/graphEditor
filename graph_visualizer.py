9f7b6d1b9c025b7460dfb2609a8f3c6ed5411158
Tanishq Sharma <ponasanthem@gmail.com>
2025-09-25 23:15:46 +0530

    coded for mtpltLib figure.

--- graph_visualizer.py
+++ graph_visualizer.py
@@ -11,4 +11,21 @@ class GraphVisualizer:
         self.pos = nx.spring_layout(graph)
         self.visited_nodes = []
 
+        # Create matplotlib figure
+        self.fig, self.ax = plt.subplots(figsize=(5,4))
+        self.canvas = FigureCanvasTkAgg(self.fig, master=root)
+        self.canvas.get_tk_widget().pack()
+
+        self.reset()
+
+    
+    def draw(self):
+        self.ax.clear()
+        nx.draw(
+            self.graph, self.pos, ax=self.ax,
+            with_labels=True,
+            node_color=["green" if n in self.visited_nodes else "skyblue" for n in self.grpah.nodes],
+            edge_color="black"
+        )
+        self.canvas.draw()
         
