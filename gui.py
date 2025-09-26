5292343ec8c42a8038f2d3cd6681c2fe1e95ee3c
Tanishq Sharma <ponasanthem@gmail.com>
2025-09-26 22:30:11 +0530

    updated code for GUI

--- gui.py
+++ gui.py
@@ -22,3 +22,19 @@ edges = []
 node_radius = 25
 dragging_node = None
 node_id_corner = 0
+
+# Toolbar State
+current_tool = None
+tools = {"Add Node", "Remove Node", "Add Edge", "Remove Edge"}
+buttons = {}
+
+def drawToolbar():
+    font = pygame.font.SysFont(None, 24)
+    x_offset = 10
+    for tool in tools:
+        rect = pygame.Rect(x_offset, 5, 120, TOOLBAR_HEIGHT - 10)
+        buttons[tool] = rect
+        pygame.draw.rect(WIN, GRAY if current_tool != tool else GREEN, rect)
+        text = font.render(tool, True, BLACK)
+        WIN.blit(text, (rect.x + 15, rect.y + 10))
+        x_offset += 130
